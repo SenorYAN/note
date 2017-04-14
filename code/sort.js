d94f15e2681372d4e9f9f00fd931f85ac9f8fa31
maoyan <1092459508@qq.com>
2017-04-14 20:33:15 +0800

    算法-2

--- code/sort.js
+++ code/sort.js
@@ -58,31 +58,27 @@ console.log('二分法：', binarySearch([1,18,8,7,8,8,11,8,8,8,8,8],8));
 
 //冒泡排序
 function bubbleSort(arr) {
-    for(var i=0, length=arr.length; i<length; i++ ){
-        var flag = true;
-        for(var j=0; j<length-i; j++){
+    arr.forEach(function(e, i){
+        for(var j=0,length=arr.length; j<length-i; j++){
             if(arr[j] > arr[j+1]){
-                flag = false;
                 var temp = arr[j];
                 arr[j] = arr[j+1];
                 arr[j+1] = temp;
             }
         }
-        if(flag){
-            break;  //发现没有逆序大小，第二轮就不用排了
-        }
-    }
+    })
+
     return arr
 }
 
-console.log('冒泡排序：', bubbleSort([12,52,63,1,50,0,13,4,6,89]));
+console.log('冒泡排序：', bubbleSort([23,0,32,45,56,75,43,0,34]));
 
 
 //简单选择排序
 function simpleSelectSort(arr) {
-    for(var i=0, length=arr.length; i<length; i++){
+    arr.forEach(function(e, i){
         var pos= i;
-        for(var j=i+1; j<length; j++){
+        for(var j=i+1, length=arr.length; j<length; j++){
             if(arr[pos] > arr[j]){
                 pos = j;
             }
@@ -90,10 +86,69 @@ function simpleSelectSort(arr) {
         var temp = arr[i];
         arr[i] = arr[pos];
         arr[pos] = temp;
-    }
+    })
     return arr;
 }
 
-console.log('简单选择排序：', simpleSelectSort([12,52,63,1,50,0,13,4,6,89]));
+console.log('简单选择排序：', simpleSelectSort([23,0,32,45,56,75,43,0,34]));
+
+//直接插入排序
+function directInsertSort(arr) {
+    arr.forEach(function(e, i){
+        var key = arr[i];
+        var j = i-1;
+        while (j>-1 && arr[j] > key){
+            arr[j+1] = arr[j];
+            j = j-1;
+        }
+        arr[j+1] = key;
+    })
+    return arr;
+}
+
+console.log('直接插入排序：', directInsertSort([23,0,32,45,56,75,43,0,34]));
+
+//快速排序
+function quickSort(arr) {
+    if(arr.length <= 1) return arr;
+    var mid = Math.floor(arr.length / 2);
+    var midVal = arr.splice(mid, 1)[0];
+    var left =[], right=[];
+    arr.forEach(function(e, i){
+        if(e < midVal){
+            left.push(e);
+        }else{
+            right.push(e);
+        }
+    })
+    return quickSort(left).concat([midVal], quickSort(right));
+}
+
+console.log('快速排序：', quickSort([23,0,32,45,56,75,43,0,34]));
+
+// 希尔排序
+function shellSort(arr) {
+    //确定间隔
+     var length = arr.length,
+         h = 1;
+     while(length/3 > h){
+         h = 3*h+1;
+     }
+     while(h>=1){
+         for(var i=h; i<length; i++){
+             for(var j=i; j>=h && arr[j] < arr[j-h]; j-=h){
+                 var temp = arr[j-h];
+                 arr[j-h] = arr[j];
+                 arr[j] = temp;
+             }
+         }
+         h = (h-1)/3;
+     }
+     return arr;
+}
+
+console.log('希尔排序：', shellSort([23,0,32,45,56,75,43,0,34]));
+
+
 
 
